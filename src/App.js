7db8b9694bf891971f360a7a3664970995a9acd5
Junho Moon <112556572+ProdMoon@users.noreply.github.com>
2023-02-20 23:06:01 +0900

    :lipstick: Implemented UI including dark mode.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import {
+  Button,
+  TextField,
+  Chip,
+  Typography,
+  Switch,
+  FormGroup,
+  FormControlLabel,
+} from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import "./App.css";
 
 function App() {
+  const [names, setNames] = useState([]);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const lightTheme = createTheme({
+    palette: {
+      mode: "light",
+    },
+  });
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
+
+  useEffect(() => {
+    let getStoredMode = localStorage.getItem("isDarkMode");
+    if (getStoredMode === null) {
+      localStorage.setItem("isDarkMode", false);
+    }
+    setIsDarkMode(getStoredMode === "true" ? true : false);
+  }, []);
+
+  function handleShuffle() {
+    let arr = [];
+    arr = arr.concat(document.getElementById("nameInput").value.split(","));
+    arr.sort(() => Math.random() - 0.5);
+    setNames(arr);
+  }
+
+  function toggleTheme() {
+    localStorage.setItem("isDarkMode", !isDarkMode);
+    setIsDarkMode(!isDarkMode);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+      <CssBaseline />
+      <div className="theme-switch">
+        <FormGroup>
+          <FormControlLabel
+            control={<Switch checked={isDarkMode} onChange={toggleTheme} />}
+            label={isDarkMode ? "Dark Mode" : "Light Mode"}
+          />
+        </FormGroup>
+      </div>
+      <div className="main">
+        지금 섞어야 하니까,
+        <Typography variant="h2">Shuffler.</Typography>
+        <div className="inputbox">
+          <TextField
+            fullWidth
+            id="nameInput"
+            variant="outlined"
+            defaultValue={"문준호,박경준,신치우,정나린,최다봄"}
+          />
+        </div>
+        <div className="shuffle-button">
+          <Button variant="contained" onClick={handleShuffle}>
+            Shuffle!
+          </Button>
+        </div>
+        <div className="chips-container">
+          {names.map((name, i) => (
+            <div className="chips" key={name}>
+              <Chip label={`${i + 1}. ${name}`} size="medium" />
+            </div>
+          ))}
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
 
