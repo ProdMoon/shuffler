01ff9487c7a6958f5c56dca52c6119c8fe7ea915
Junho Moon <112556572+ProdMoon@users.noreply.github.com>
2023-02-21 00:44:55 +0900

    Improved shuffle algorithm

--- src/App.js
+++ src/App.js
@@ -39,7 +39,10 @@ function App() {
   function handleShuffle() {
     let arr = [];
     arr = arr.concat(document.getElementById("nameInput").value.split(","));
-    arr.sort(() => Math.random() - 0.5);
+    for (let i = arr.length -1; i > 0; i--) {
+      let j = Math.floor(Math.random()*(i+1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
     setNames(arr);
   }
 
